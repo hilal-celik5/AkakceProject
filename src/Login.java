93bb7b3ce5824118de2b39fe5a27645d9368f538
rukiye <bakachanrukiye@gmail.com>
2025-03-18 02:00:09 +0300

    Log in sayfası güncellendi

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,6 @@
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -7,20 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Wait;
 
 public class Login extends BaseDriver {
-    public static void main(String[] args) {
 
-        WebDriver tarayici=new ChromeDriver();
-        tarayici.get("https://www.akakce.com/akakcem/giris/");
+    @Test
+    public void Test1() {
+        driver.get("https://www.akakce.com/akakcem/giris/");
+        WebElement eMail = driver.findElement(By.id("life"));
+        eMail.sendKeys("vokiwec222@dmener.com");
 
-        WebElement eMail= tarayici.findElement(By.id("life"));
-        eMail.sendKeys(":vokiwec222@dmener.com");
-
-        WebElement password= tarayici.findElement(By.id("lifp"));
+        WebElement password = driver.findElement(By.id("lifp"));
         password.sendKeys("Password123");
 
-        MyFunc.Wait(2);
-        tarayici.quit();
+        WebElement submitButton = driver.findElement(By.id("lfb"));
+        submitButton.click();
+
+        tearDown();
+
 
     }
-
 }
