91a2d2dbda8553e2611923a971a49c190bdcda82
mustafainan5858@gmail.com <mustafainan5858@gmail.com>
2025-03-18 22:25:45 +0300

    hesap dogrulama hazırladım onaylar mısın

--- src/AccountVerification.java
+++ src/AccountVerification.java
@@ -1,4 +1,33 @@
 import Utility.BaseDriver;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import static Utility.MyFunc.Wait;
 
 public class AccountVerification extends BaseDriver {
+    @Test//main yerine
+    public void Test1() {
+        driver.get("https://www.akakce.com/akakcem/giris/");
+
+        WebElement email = driver.findElement(By.cssSelector("input[id='life']"));
+        email.sendKeys("vokiwec222@dmener.com");
+
+        WebElement password = driver.findElement(By.cssSelector("input[type='password']"));
+        password.sendKeys("Password123");
+
+        WebElement submit = driver.findElement(By.cssSelector("[type='submit'][class='s'][value='Giriş yap']"));
+        submit.click();
+
+        WebElement shoppingCartMesaj = driver.findElement(By.cssSelector("[rel='nofollow'][href='/akakcem/']"));
+        shoppingCartMesaj.click();
+
+        WebElement profilismi = driver.findElement(By.cssSelector("[rel='nofollow'][href='/akakcem/']"));
+        Assert.assertTrue("Profil ", profilismi.getText().contains("TestUser"));
+
+        Wait(3);
+        driver.quit();
+
+    }
 }
