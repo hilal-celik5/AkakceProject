9f47ac287f29db34736bc21bb664d813ee88e397
rukiye <bakachanrukiye@gmail.com>
2025-03-18 21:39:29 +0300

    Log in sayfası güncellendii

--- src/Login.java
+++ src/Login.java
@@ -1,5 +1,6 @@
 import Utility.BaseDriver;
 import Utility.MyFunc;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -9,17 +10,21 @@ import org.openqa.selenium.support.ui.Wait;
 
 public class Login extends BaseDriver {
 
+    private Assertions Assert;
+
     @Test
     public void Test1() {
+
         driver.get("https://www.akakce.com/akakcem/giris/");
+
         WebElement eMail = driver.findElement(By.id("life"));
         eMail.sendKeys("vokiwec222@dmener.com");
 
         WebElement password = driver.findElement(By.id("lifp"));
         password.sendKeys("Password123");
 
-        WebElement submitButton = driver.findElement(By.id("lfb"));
-        submitButton.click();
+        WebElement submit = driver.findElement(By.id("lfb"));
+        submit.click();
 
         tearDown();
 
