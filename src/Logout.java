3564c9bf407b392de415d939d735eed5edfc9b28
Görkem <gorkem.kezr@gmail.com>
2025-03-19 09:19:12 +0300

    email girildi

--- src/Logout.java
+++ src/Logout.java
@@ -1,5 +1,19 @@
 import Utility.BaseDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 public class Logout extends BaseDriver {
+
+    @Test
+
+    public void Test1(){
+
+        driver.get("https://www.akakce.com/akakcem/giris/");
+
+        WebElement email = driver.findElement(By.cssSelector("input[id='life']"));
+        email.sendKeys(("vokiwec222@dmener.com"));
+
+
+    }
 
 }
