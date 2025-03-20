8394003182901a21e47c7f3dfb4ebfbc1410c733
Görkem <gorkem.kezr@gmail.com>
2025-03-21 00:15:59 +0300

    logout yapıldı

--- src/Logout.java
+++ src/Logout.java
@@ -1,19 +1,32 @@
 import Utility.BaseDriver;
+import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 
 public class Logout extends BaseDriver {
 
     @Test
+    public void Testexit() {
+        driver.get("https://www.akakce.com/");
 
-    public void Test1(){
+        WebElement login = driver.findElement(By.cssSelector("[rel='nofollow'][href='/akakcem/giris/']"));
+        login.click();
 
-        driver.get("https://www.akakce.com/akakcem/giris/");
+        WebElement email = driver.findElement(By.cssSelector("[id='life']"));
+        email.sendKeys("vokiwec222@dmener.com");
 
-        WebElement email = driver.findElement(By.cssSelector("input[id='life']"));
-        email.sendKeys(("vokiwec222@dmener.com"));
+        WebElement password = driver.findElement(By.cssSelector("[id='lifp']"));
+        password.sendKeys("Password123");
 
+        WebElement submit = driver.findElement(By.cssSelector("[id='lfb']"));
+        submit.click();
 
+        WebElement account = driver.findElement(By.cssSelector("[id='HM_v8']>i>a"));
+        account.click();
+
+        WebElement exit = driver.findElement(By.cssSelector("[href='#Çık']"));
+        exit.click();
+
+        tearDown();
     }
-
 }
